4b5ef222b1fd416fef3b5e73c1f32a845dd68c57
magex9 <magex@magex.ca>
2016-01-21 11:52:22 -0500

    Updates

---
+++ release.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+export SVNBASE="https://github.com/magex9/build.git"
+echo Using svn base: $SVNBASE
+
+echo "Checking for $SVNBASE/trunk"
+svn ls $SVNBASE 2>/dev/null | grep -icq "trunk"
+if [[ $? -eq 0 ]]; then
+	echo "[P] Trunk exists"
+else
+	echo "[F] Trunk doesnt exist"
+fi
+
+echo "Checking for $SVNBASE/trunk"
+svn ls $SVNBASE 2>/dev/null | grep -icq "tags"
+if [[ $? -eq 0 ]]; then
+        echo "[F] Tags exists"
+else
+        echo "[P] Tags doesnt exist"
+fi
+
+export BUILD_WORK=target/work
+if [ -d $BUILD_WORK ]; then
+	rm -rf $BUILD_WORK
+fi
+
+svn checkout $SVNBASE/trunk $WORKSPACE
+
+export MAVEN_OPTS="-Dmaven.repo.local=target/local-repo -DaltDeploymentRepository=snapshot-repo::default::file:target/snapshot-repo -DaltDeploymentRepository=release-repo::default::file:target/release-repo"
+
+# Download the clean dependencies
+#mvn clean
+
+# Install application
+#mvn install
+
+# Deploy
+#mvn deploy $MAVEN_OPTS 
+
+# Release it
+#mvn release:prepare
+
+# Deploy the release
+#mvn release:perform
+
--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-# Download the clean dependencies
-mvn clean
+export SVNBASE="https://github.com/magex9/build.git"
+export BUILD_WORK=target/work
+if [ -d $BUILD_WORK ]; then
+	rm -rf $BUILD_WORK
+fi
 
-# Install application
-mvn install
+svn checkout $SVNBASE/trunk $BUILD_WORK
+
+export MAVEN_OPTS="-Dmaven.repo.local=target/local-repo -DaltDeploymentRepository=release-repo::default::file:target/deploy-repo"
+
+cd $BUILD_WORK
+pwd
+# Deploy
+mvn clean deploy 
 
 # Release it
-mvn release:prepare
+mvn --batch-mode release:prepare
 
 # Deploy the release
-mvn release:perform
+#mvn release:perform
 
