e067a82d76e7a611c2c49dd9bf88fb3fef5009f8
magex9 <magex@magex.ca>
2016-01-21 14:05:33 -0500

    Added plugin example

---
+++ builder-info-plugin/src/main/java/ca/magex/maven/plugin/BuildInfoMojo.java
@@ -0,0 +1,43 @@
+package ca.magex.maven.plugin;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.util.FileUtils;
+
+/**
+ * Says "Hi" to the user.
+ *
+ */
+@Mojo(name = "sayhi")
+public class BuildInfoMojo extends AbstractMojo {
+
+	@Component
+	protected MavenProject project;
+	
+	@Component
+	protected MavenProjectHelper projectHelper;
+
+	@Component
+	private ArtifactResolver artifactResolver;
+
+	public void execute() throws MojoExecutionException {
+		getLog().info("Hello, world.");
+
+		try {
+			FileUtils.fileWrite("target/props.data", "test");
+		} catch (IOException e) {
+			throw new MojoExecutionException("Error creating file", e);
+		}
+		
+		projectHelper.attachArtifact(project, new File("target/props.data"), "build-info");
+
+	}
+}
