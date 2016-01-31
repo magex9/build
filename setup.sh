b131e4b9e4aea685acb8935d4de18c2ef187bf1a
magex9 <magex@magex.ca>
2016-01-31 13:35:42 -0500

    uncommented commands

--- setup.sh
+++ setup.sh
@@ -32,11 +32,11 @@ mvn --batch-mode release:prepare release:perform
 mvn site
 
 # Useful commands
-#mvn dependency:tree eclipse:eclipse findbugs:findbugs cobertura:cobertura
+mvn dependency:tree eclipse:eclipse findbugs:findbugs cobertura:cobertura
 
 # Create an archetype
 cd target
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DgroupId=ca.magex.archetype -DartifactId=mywebapp -Dversion=1.0.0-SNAPSHOT -Dpackage=ca.magex.archetype
+mvn --batch-mode archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DgroupId=ca.magex.archetype -DartifactId=mywebapp -Dversion=1.0.0-SNAPSHOT -Dpackage=ca.magex.archetype
 
 # Zip up required content
 rm -rf $WORKSPACE/local-repo/ca
