63c532e3dbf0270e53fdc8ced448da0ca7fef823
magex9 <magex@magex.ca>
2016-03-04 09:44:00 -0500

    Added the maven exception

--- builder-maven-plugin/src/main/java/ca/magex/maven/exceptions/MavenException.java
+++ builder-maven-plugin/src/main/java/ca/magex/maven/exceptions/MavenException.java
@@ -4,7 +4,7 @@ package ca.magex.maven.exceptions;
  * A generic catch all for thrown exceptions to become runtime.
  * 
  * This should be used when exception could happen, but it should be handled at
- * a generic statement in the servlet/main class.
+ * a generic statement in the servlet/main class/maven mojo.
  * 
  * These will be all things that will result in a failed build and do not need
  * to be handled in a specific way.
@@ -12,15 +12,15 @@ package ca.magex.maven.exceptions;
  * @author magex
  *
  */
-public class BuilderException extends RuntimeException {
+public class MavenException extends RuntimeException {
 
 	private static final long serialVersionUID = 928948923492843L;
 
-	public BuilderException(String msg) {
+	public MavenException(String msg) {
 		super(msg);
 	}
 
-	public BuilderException(String msg, Exception e) {
+	public MavenException(String msg, Exception e) {
 		super(msg, e);
 	}
 
