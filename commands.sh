e648c32e8661348a05ce33da13fb1180100d53cc
magex9 <magex@magex.ca>
2016-03-12 14:37:24 -0500

    Added comments

--- commands.sh
+++ commands.sh
@@ -4,38 +4,54 @@ echo Using workspace: $WORKSPACE
 echo Using svn base: $SVNBASE
 
 export CHECKOUT=$WORKSPACE/checkout
+echo Deleting working directory $CHECKOUT
 if [ -d $CHECKOUT ]; then
 	rm -rf $CHECKOUT
 fi
 
+echo Checkout $SVNBASE/trunk to $CHECKOUT
 svn checkout $SVNBASE/trunk $CHECKOUT
 
 export MAVEN_OPTS="-Dmaven.repo.local=$WORKSPACE/local-repo -DaltDeploymentRepository=release-repo::default::file:$WORKSPACE/deploy-repo"
+echo MAVEN_OPTS=$MAVEN_OTPS
 
 cd $CHECKOUT
-echo Working directory:
+echo Present working directory:
 pwd
 
 # Deploy
+echo Maven clean and deploy
 mvn clean deploy 
 
 # Release it
+echo Maven release using default options
 mvn --batch-mode release:prepare release:perform
 
 # Site
+echo Build html report about the project
 mvn site
 
 # Useful commands
-mvn dependency:tree eclipse:eclipse findbugs:findbugs cobertura:cobertura
-
-# Run Jacoco
-mvn mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
+echo Display a dependency tree
+mvn dependency:tree 
+echo Download the eclipse plugin
+mvn eclipse:eclipse 
+echo Download the findbugs plugin
+mvn findbugs:findbugs 
+echo Download the cobertura plugin
+mvn cobertura:cobertura
+echo Download the jacoco plugin
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true
 
 # Create an archetype
 cd target
+echo Creating a webapp archetype
 mvn --batch-mode archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DgroupId=ca.magex.archetype -DartifactId=mywebapp -Dversion=1.0.0-SNAPSHOT -Dpackage=ca.magex.archetype
 
 # Zip up required content
+echo Deleting builder artifacts from final repo
 rm -rf $WORKSPACE/local-repo/ca
+
+echo Zipping repository into $WORKSPACE/maven-central.tar.gz
 tar -c $WORKSPACE/local-repo | gzip > $WORKSPACE/maven-central.tar.gz
 
